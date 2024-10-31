4a6a071c642f413a7fad6d6bd83c75ad665396b0
Noah <91566343+ItsFireninja@users.noreply.github.com>
2024-10-31 11:55:34 -0500

    Added Encoder Code

--- AutonForRightField.java
+++ AutonForRightField.java
@@ -24,35 +24,79 @@ public class AutonForRightField extends LinearOpMode {
         DcMotor leftSlide = hardwareMap.get(DcMotor.class,"leftSlide");
         DcMotor rightSlide = hardwareMap.get(DcMotor.class,"rightSlide");
         DcMotor Arm = hardwareMap.get(DcMotor.class,"Arm");
+        //Not even sure it runs
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //More code
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+        if (isStopRequested()) return;
+        //I'll test this out l8r
+        moveWheel()
+
     }
     public void  moveWheel(float x,float y) {
          float fl = (y+x);
          float fr = (x-y);
          float bl = (y-x);
          float br = (-y-x);
-
+        double wheelCPR = 423.2116; //Counts per revolution
+         int Flposition = frontLeft.getCurrentPosition();
+         int Frposition = frontRight.getCurrentPosition();
+         int Blposition = backLeft.getCurrentPosition();
+         int Brposition = backRight.getCurrentPosition();
+         //YAY MORE VARIABLES
+         double Flrevolutions = Flposition/wheelCPR;
+         double Frrevolutions = Frposition/wheelCPR;
+         double Blrevolutions = Blposition/wheelCPR;
+         double Brrevolutions = Brposition/wheelCPR;
+        double leadMotor = Math.min(Math.min(Flrevolutions,Frrevolutions),Math.min(Blrevolutions,Brrevolutions));
+         //Modifier Variables YAY
+        // double linearMotor = Math.min(Lsposition,Rsposition);
+         double flModifier = 1;
+         double frModifier = 1;
+         double blModifier = 1;
+         double brModifier = 1;
+            if (Flrevolutions > leadMotor) {
+                flModifier = leadMotor/Flrevolutions;
+            }
+            if (Frrevolutions > leadMotor) {
+                frModifier = leadMotor/Frrevolutions;
+            }
+            if (Blrevolutions > leadMotor) {
+                blModifier = leadMotor/Blrevolutions;
+            }
+            if (Brrevolutions > leadMotor) {
+                brModifier = leadMotor/Brrevolutions;
+            }
         //stops it from going greater than 1/-1
          float maxNumber = Math.max(Math.abs(x)+Math.abs(y),1);
          //powers the motor for wheels
-        frontLeft.setPower(fl/maxNumber);
-        frontRight.setPower(fr/maxNumber);
-        backLeft.setPower(bl/maxNumber);
-        backRight.setPower(br/maxNumber);
+        frontLeft.setPower(fl/maxNumber*flModifier);
+        frontRight.setPower(fr/maxNumber*frModifier);
+        backLeft.setPower(bl/maxNumber*blModifier);
+        backRight.setPower(br/maxNumber*brModifier); 
+        
     }
    public void turn(boolean rotateLeft, boolean rotateRight) {
         double fl, fr, bl, br;
         fl = fr = bl = br = 0;
         if (rotateRight) {
-            fl = 0.5;
-            fr = 0.5;
-            bl = 0.5;
-            br = 0.5;
+            fl = 1;
+            fr = 1;
+            bl = 1;
+            br = 1;
          }
          if (rotateLeft) {
-             fl = -0.5;
-             fr = -0.5;
-             bl = -0.5;
-             br = -0.5;
+             fl = -1;
+             fr = -1;
+             bl = -1;
+             br = -1;
          }
     } 
     public void linearVertical(float verticalPower) {
